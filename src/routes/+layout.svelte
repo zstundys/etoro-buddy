3990d4639f5a6cb318dd7c62829be40ce5c28c6c
Žanas Stundys <z.stundys@gmail.com>
2026-02-27 23:58:29 +0200

    Replace SVG favicon with JPEG version for improved compatibility and update layout to reflect the change in file type.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+    import favicon from '$lib/assets/favicon.jpg';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" type="image/jpeg" href={favicon} />
 	<title>eToro Buddy</title>
 </svelte:head>
 
